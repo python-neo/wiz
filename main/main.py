be136729e048789d085823a95e9d3991f52a82d2
Aarav Agarwal <279409447+python-neo@users.noreply.github.com>
2026-03-27 12:06:23 +0530

    Added "list" command

--- main/main.py
+++ main/main.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser, REMAINDER
 from textwrap import dedent
+from pathlib import Path
+from json import dump, load
 
 HELP_TEXT = dedent (
     """
@@ -9,15 +11,31 @@ HELP_TEXT = dedent (
         add             Add an alias to the manager
         delete          Delete an alias from the manager
         run             Run given alias
+        list            List all aliases
         help            Show help for commands
     """).strip ()
 
 class Commands :
     def __init__ (self) :
         self.commands = {
+            "list" : {"func" : self.list, "subcmd" : False, "rest" : False},
             "help" : {"func" : self.help, "subcmd" : False, "rest" : False},
         }
-        
+        self.BASE_DIR = Path (__file__).resolve ().parent
+        self.config_path = self.BASE_DIR / "config.json"
+
+        if not self.config_path.exists () :
+            with open (self.config_path, "w") as f :
+                dump ({}, f, indent = 4)
+
+    def list (self) :
+        with open (self.config_path, "r") as f :
+            data = load (f)
+            if not data :
+                return
+        for alias, command in data.items () :
+            print (f"{alias}        {command}")
+
     def help (self) :
         print (HELP_TEXT)
 
